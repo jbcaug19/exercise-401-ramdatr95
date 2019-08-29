bd005f12ac744ac265b8ed879270125e33f8fd11
Ramdatr95 <Ryan.T.Ramdat@gmail.com>
2019-08-29 11:15:03 -0400

    exercise 402

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -22,7 +22,7 @@ WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests().anyRequest().authenticated()
-                .and().formLogin();
+                .and().formLogin().loginPage("/login").permitAll();
     }
 
     @Override
